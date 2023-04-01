f5a9cd6e9b4f27bbd6767786737e348d1b841f42
danifromecuador <danifromecuador@gmail.com>
2023-03-31 23:27:51 -0500

    Revert "To Do list: interactive list"

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import {
   add, render, remove, edit,
 } from './modules/functions.js';
 
-import { TODO, clearAll } from './modules/interactive.js';
-
 // ADD A NEW TASK
 const addButton = document.querySelector('.add-button'); // clicking add button
 addButton.addEventListener('click', () => {
@@ -22,6 +20,7 @@ addTask.addEventListener('keydown', (event) => {
 
 // DELETE A TASK
 const tasksContainer = document.querySelector('.tasks-container');
+
 tasksContainer.addEventListener('click', (event) => {
   const deleteTaskIcon = event.target.closest('.delete-task-icon');
   if (deleteTaskIcon) {
@@ -40,25 +39,6 @@ tasksContainer.addEventListener('click', (event) => {
     const index = Array.from(textInputs).indexOf(textInput);
     edit(index);
   }
-});
-
-// STRIKETHROUGH A COMPLETED TASK
-const todo = new TODO();
-tasksContainer.addEventListener('click', (event) => {
-  const checkBox = event.target.closest('.checkbox-input');
-  if (checkBox) {
-    const checkBoxes = tasksContainer.querySelectorAll('.checkbox-input');
-    const index = Array.from(checkBoxes).indexOf(checkBox);
-    todo.strikeThrough(index);
-    render();
-  }
-});
-
-// CLEAR ALL COMPLETED TASKS
-const clearAllButton = document.querySelector('.clear-all-completed');
-clearAllButton.addEventListener('click', () => {
-  clearAll();
-  render();
 });
 
 // FIRST RENDER WHEN THE PAGE LOADS
